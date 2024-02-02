c57c72781baa5b79a8a777ca67447da47afd5663
stephen saidimu <stevensaidimu@gmail.com>
2024-02-02 21:02:35 +0300

    first commit

--- app/lib/mongodb.js
+++ app/lib/mongodb.js
@@ -5,9 +5,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     if (mongoose.connection.readyState === 0) {
-      await mongoose.connect(
-        "mongodb+srv://marryanne:c8MOzqj4n5QC8bRk@cluster0.def3tdg.mongodb.net/sherryhomes"
-      );
+      await mongoose.connect();
       console.log("db connected");
     }
   } catch (error) {
